6edc840d168bbf4f2acaa510546bdfb95e335b15
AldisG <aldisgailis1@gmail.com>
2023-01-17 21:41:07 +0200

    more setup

---
+++ codnity-page/src/components/WelcomeComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+// const someValue = ref(0);
+</script>
+
+<template>
+  <div>Welcme screen</div>
+</template>
+
+<style lang=""></style>
---
+++ codnity-page/src/views/LoginPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import WelcomeComponent from "@/components/WelcomeComponent.vue";
+import LoginWrapper from "@/components/LoginWrapper.vue";
+</script>
+
+<template>
+  <main>
+    <WelcomeComponent />
+    <LoginWrapper />
+  </main>
+</template>
